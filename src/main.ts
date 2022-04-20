f872704da4198b5d8da6917520cd7715e83208ad
悟能 <15660822700@163.com>
2022-04-20 22:08:35 +0800

    feat:初始化样式

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './mock/mockServer';
-
+import 'normalize.css'
+import '.././src/styles/common.scss'
 createApp(App).use(ElementPlus).use(store).use(router)
   .mount('#app');
